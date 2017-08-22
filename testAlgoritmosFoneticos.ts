1580873a2d79f2f671a4df2c2b1319e9cd3d73cd
jfgabriel <jgabriel@neuquen.gov.ar>
2017-08-22 17:04:57 -0300

    remove console.log

--- testAlgoritmosFoneticos.ts
+++ testAlgoritmosFoneticos.ts
@@ -4,7 +4,9 @@ import { IWeight } from './lib/IWeight';
 import { MetaphoneES } from './lib/MetaphoneES';
 import { SoundexES } from './lib/SoundexES';
 
+// tslint:disable-next-line:no-console
 console.log(distance('Natalia', 'Carina', { caseSensitive: false }));
 
 let algSoundex = new SoundexES();
+// tslint:disable-next-line:no-console
 console.log(algSoundex.soundex('Abadia'));
